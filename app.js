6513550070690df9a8cd57b1f43b9c0435de1a12
mariele <mariele_rbr@hotmail.com>
2021-02-04 16:53:20 -0300

    post filtering

--- app.js
+++ app.js
@@ -62,3 +62,23 @@ window.addEventListener('scroll', () => {
     showLoader()
   }
 })
+
+// FILTRAGEM DE POSTS 
+
+filterInput.addEventListener('input', event => {
+  const inputValue = event.target.value.toLowerCase()
+  const posts = document.querySelectorAll('.post')
+
+  posts.forEach(post => {
+    const postTitle = post.querySelector('.post-title').textContent.toLowerCase()
+    const postBody = post.querySelector('.post-body').textContent.toLowerCase()
+
+    if (postTitle.includes(inputValue) || postBody.includes(inputValue)) {
+      post.style.display = 'flex'
+      return
+    }
+
+    post.style.display = 'none'
+
+  })
+})
